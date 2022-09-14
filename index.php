7fc388da20d2b857b7109ae6241dc3d98b7cb884
gfftr <gff.terl@gmail.com>
2022-09-14 23:55:28 +0100

    delete & change php added

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
+<?php
+
+$todo = [];
+if (file_exists('todo.json')) {
+ $json = file_get_contents('todo.json');
+ $todos = json_decode($json, true);
+}
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -13,6 +24,40 @@
   <input type="text" name="todo_name" placeholder="Enter your todo">
   <button>New Todo</button>
  </form>
+ <br>
+ <?php
+
+ foreach ($todos as $todoName => $todo) : ?>
+
+ <div style="margin-bottom: 20px;">
+
+  <form style="display: inline" action="change_status.php" method="post">
+   <input type="hidden" name="todo_name" value="<?php echo $todoName ?>">
+   <input type="checkbox" <?php echo $todo['completed'] ? 'checked' : ''; ?>>
+  </form>
+
+
+  <?php echo $todoName ?>
+
+  <form style="display: inline" action="delete.php" method="post">
+   <input type="hidden" name="todo_name" value="<?php echo $todoName ?>">
+   <button>Delete</button>
+  </form>
+
+ </div>
+
+ <?php
+ endforeach;
+ ?>
+
+ <script>
+ const checkboxes = document.querySelectorAll('input[type=checkbox]');
+ checkboxes.forEach(ch => {
+  ch.onclick = function() {
+   this.parentNode.submit();
+  };
+ })
+ </script>
 </body>
 
 </html>
--- newtodo.php
+++ newtodo.php
@@ -9,5 +9,20 @@ $todoName = trim($todoName);
 
 
 if ($todoName) {
- file_put_contents('todo.json');
+ if (file_exists('todo.json')) {
+  $json = file_get_contents('todo.json');
+  $jsonArray = json_decode($json, true);
+ } else {
+  $jsonArray = [];
+ }
+
+ $jsonArray[$todoName] = ['completed' => false];
+
+ // echo '<pre>';
+ // var_dump($jsonArray);
+ // echo '</pre>';
+
+ file_put_contents('todo.json', json_encode($jsonArray));
 }
+
+header('Location: index.php');
